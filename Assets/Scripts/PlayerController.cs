a2dd8b304a09dcb48b3d19100a8c6099e56c9f17
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-09-19 21:33:22 -0400

    Add PlayerController to PartyWitch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Scripts : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     Vector2 inputDir;
     PlayerControls playerControls;

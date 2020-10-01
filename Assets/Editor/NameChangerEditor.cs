d74b338d46db1b18aaa2bc20494a64d8f1d373d6
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-10-01 06:41:29 -0400

    Add name changer tool

---
+++ Assets/Editor/NameChangerEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(NameChanger))]
+public class NameChangerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        if (GUILayout.Button("Change name"))
+        {
+            ((NameChanger)target).changeName();
+        }
+    }
+}
---
+++ Assets/Scripts/Tools/NameChanger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Tool used to mass-change a character's name in DialogueTriggers
+/// </summary>
+public class NameChanger : MonoBehaviour
+{
+
+    public string oldName;
+    public string newName;
+
+    public void changeName()
+    {
+        List<DialogueTrigger> dialogueTriggers = FindObjectsOfType<DialogueTrigger>()
+            .Where(d => d.characters.Contains(oldName)).ToList();
+        if (dialogueTriggers.Count == 0)
+        {
+            Debug.Log("Unable to find any DialogueTriggers with the name " + oldName);
+            return;
+        }
+        Undo.RecordObjects(
+            dialogueTriggers.ToArray(),
+            "Change name: " + oldName + " -> " + newName
+            + " (x" + dialogueTriggers.Count + ")"
+            );
+        dialogueTriggers.ForEach(
+            d =>
+            {
+                for (int i = 0; i < d.characters.Count; i++)
+                {
+                    if (d.characters[i] == oldName)
+                    {
+                        d.characters[i] = newName;
+                    }
+                }
+            }
+            );
+    }
+}

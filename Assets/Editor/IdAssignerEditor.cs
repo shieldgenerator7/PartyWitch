609f0db975f267daea7366b66e1cfcd58cd7eb7d
shieldgenerator7 <shieldgenerator7@yahoo.com>
2020-10-02 17:14:55 -0400

    Add Id Assigner tool

---
+++ Assets/Editor/IdAssignerEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(IdAssigner))]
+public class IdAssignerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Assign Ids"))
+        {
+            ((IdAssigner)target).assignIds();
+        }
+    }
+}

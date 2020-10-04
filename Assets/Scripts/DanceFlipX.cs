79e9df23ab1d53aa84835b310a05272407e24907
Darrias <38997264+HyperspaceBattle@users.noreply.github.com>
2020-10-03 21:24:52 -0400

    Dancing Party Animations

--- Assets/Scripts/DanceFlipX.cs
+++ Assets/Scripts/DanceFlipX.cs
@@ -8,59 +8,100 @@ public class DanceFlipX : MonoBehaviour
 	public float randomMin	= 0f;
 	public float randomMax = 5f;
 	public float fixedRate	= 1f;
-	private float timeRemaining = 10f;
-	public float timeRemainingStored = 10f;
+	private float timeRemaining = .5f;
+	private float timeRemainingFixed = 1f;
 	private bool FacingRight = true;  // For determining which way the player is currently facing.
+	
+	public bool spriteSwap = false; 
+	private bool isSprite01 = true;
+	public SpriteRenderer spriteRenderer;
+	public Sprite sprite01;
+	public Sprite sprite02;
+	
+	void Start(){
+		timeRemainingFixed = fixedRate;
+	}
 	
     // Update is called once per frame
     void Update()
-    {
-		if (timeRemaining > 0)
-		{
-			timeRemaining -= Time.deltaTime;
-		}
-		else
-		{
-			Flip();
-		}
-		
+    {	
+		//random
 		if (random)
 		{
+			//countdown
 			if (timeRemaining > 0)
 			{
 				timeRemaining -= Time.deltaTime;
 			}
+			//flip reset to random
 			else
 			{
-				Flip();
-				timeRemaining = Random.Range(randomMin, randomMax);
+				if (spriteSwap)
+				{
+					if (isSprite01)
+					{
+						spriteRenderer.sprite = sprite02; 
+						isSprite01 = !isSprite01;
+						timeRemaining = Random.Range(randomMin, randomMax);
+					}
+					else
+					{
+						spriteRenderer.sprite = sprite01; 
+						isSprite01 = !isSprite01;
+						timeRemaining = Random.Range(randomMin, randomMax);
+					}
+				}
+				else
+				{
+					Flip();
+					timeRemaining = Random.Range(randomMin, randomMax);
+				}
+				
 			}	
 		}	
-		else
+		//not random
+		else if (!random)
 		{
-		
+			//countdown
 			if (timeRemaining > 0)
-				{
-					timeRemaining -= Time.deltaTime;
-				}
+			{
+				timeRemaining -= Time.deltaTime;
+			}
+			//flip reset to random	
 			else
+			{
+				if (spriteSwap)
 				{
-				Flip();
-					timeRemaining = timeRemainingStored;
+					if (isSprite01)
+					{
+						spriteRenderer.sprite = sprite02; 
+						isSprite01 = !isSprite01;
+						timeRemaining = timeRemainingFixed;
+					}
+					else
+					{
+						spriteRenderer.sprite = sprite01; 
+						isSprite01 = !isSprite01;
+						timeRemaining = timeRemainingFixed;
+					}
 				}
+				else
+				{
+					Flip();
+					timeRemaining = timeRemainingFixed;
+				}
+			}
 		}
-		
-
-    }
 	
-    private void Flip()
-    {
-        // Switch the way the player is labelled as facing.
-        FacingRight = !FacingRight;
-
-        // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+		void Flip()
+		{
+			// Switch the way the player is labelled as facing.
+			FacingRight = !FacingRight;
+			
+			// Multiply the player's x local scale by -1.
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+		}
+	}
 }
